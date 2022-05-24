b37747e0c387f2dfe746bcbe10b1cafb1ced3be0
nasser <anasserabdrabo@gmail.com>
2022-05-25 00:38:36 +0200

    feat: :fire: add reset counter

---
+++ src/views/ResetCounter.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="about">
+    <h1>Counter Reset</h1>
+  </div>
+</template>
+
+<script>
+import { initializeApp } from "firebase/app";
+import { getDatabase, set, ref } from "firebase/database";
+
+export default {
+  mounted() {
+    const firebaseConfig = {
+      apiKey: process.env.VUE_APP_API_KEY,
+      authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+      databaseURL: process.env.VUE_APP_DATABASE_URL,
+      projectId: process.env.VUE_APP_PROJECT_ID,
+      storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+      messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
+      appId: process.env.VUE_APP_APP_ID,
+    };
+
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    const database = getDatabase(app);
+
+    set(ref(database, "ratings"), {});
+  },
+};
+</script>
